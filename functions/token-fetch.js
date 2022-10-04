315f7ee430674fe5bd9c942306f6b45996c765bc
buidohuy99 <huy.buido@haravan.com>
2022-10-04 20:09:48 +0700

    Update to bot message

--- functions/token-fetch.js
+++
@@ -1,5 +0,0 @@
-const axios = require('axios').default;
-
-module.exports.retrieveTwitchAccessToken = () => {
-    
-}
--- services/connected-event-processor.js
+++ services/connected-event-processor.js
@@ -1,15 +1,15 @@
-const axios = require('./auth-axios');
+const axios = require('../functions/auth-axios');
 
 module.exports = (address, port, client) => {
     sippingInterval = setInterval(async () => {
         if(process.env.APP_ENV != 'production') return;
-        const {data: response} = await axios.get(process.env.TWITCH_API_URL + `/helix/streams?user_login=${process.env.TWITCH_CHANNEL}`);
         
         const currentDate = new Date();
         if(currentDate.getMinutes() % 30 != 0) return;
-        if(response.data && response.data.length != 0) return;
-        
 
-        client.say(process.env.TWITCH_CHANNEL, "eggySip");
+        const {data: response} = await axios.get(process.env.TWITCH_API_URL + `/helix/streams?user_login=${process.env.TWITCH_CHANNEL}`);
+        if(response.data && response.data.length != 0) return;
+
+        client.say(process.env.TWITCH_CHANNEL, "eggySip Remember to keep yourself peepoHappy hydrated peepoHappy chat eggyDrink");
     }, 60000);
 };
