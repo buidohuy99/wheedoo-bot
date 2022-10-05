5374ae67197bf0093c79f9e4efde250edb94c50d
buidohuy99 <huy.buido@haravan.com>
2022-10-05 11:05:03 +0700

    Update logic to keep bot active

--- services/connected-event-processor.js
+++ services/connected-event-processor.js
@@ -5,9 +5,9 @@ module.exports = (address, port, client) => {
         if(process.env.APP_ENV != 'production') return;
         
         const currentDate = new Date();
-        if(currentDate.getMinutes() % 30 != 0) return;
-
         const {data: response} = await axios.get(process.env.TWITCH_API_URL + `/helix/streams?user_login=${process.env.TWITCH_CHANNEL}`);
+
+        if(currentDate.getMinutes() % 30 != 0) return;
         if(response.data && response.data.length != 0) return;
 
         client.say(process.env.TWITCH_CHANNEL, "eggySip Remember to keep yourself peepoHappy hydrated eggyDrink chat 💕");
